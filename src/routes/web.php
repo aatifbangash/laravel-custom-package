0335ce4c3b14b63af426d87587367196ea830d57
Atif Ali Khan <35859555+aatifbangash@users.noreply.github.com>
2024-02-23 15:26:32 +0500

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,5 +2,8 @@
 
 Route::group(['namespace' => 'Bitfumes\Contact\Http\Controllers'], function () {
     Route::get('contact', 'ContactController@index')->name('contact');
+    Route::get('contact2', function(){
+        dd('contact2');
+    })->name('contact2');
     Route::post('contact', 'ContactController@send');
 });
