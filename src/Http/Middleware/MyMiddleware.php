9193447df17b08eb1f86757a06e6039056967a1e
Atif Ali Khan <35859555+aatifbangash@users.noreply.github.com>
2024-02-23 15:49:43 +0500

    Update MyMiddleware.php

--- src/Http/Middleware/MyMiddleware.php
+++ src/Http/Middleware/MyMiddleware.php
@@ -1,1 +1,12 @@
+<?php
 
+namespace Bitfumes\Contact\Http\Middleware;
+
+class MyMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        dump('MyMiddleware');
+        return $next($request);
+    }
+}
