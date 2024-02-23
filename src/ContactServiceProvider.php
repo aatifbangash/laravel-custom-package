9f294f8680890753e78cd8fdf97ff9780dd57d60
Atif Ali Khan <35859555+aatifbangash@users.noreply.github.com>
2024-02-23 15:53:15 +0500

    Update ContactServiceProvider.php

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -1,16 +1,18 @@
 <?php
 
 namespace Bitfumes\Contact;
-
+use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
+use Bitfumes\Contact\Http\Middleware\MyMiddleware;
 
 class ContactServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(Router $router)
     {
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/views', 'contact');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $router->middlewareGroup('MyMiddleware', [MyMiddleware::class]);
         $this->mergeConfigFrom(
             __DIR__ . '/config/contact.php',
             'contact'
